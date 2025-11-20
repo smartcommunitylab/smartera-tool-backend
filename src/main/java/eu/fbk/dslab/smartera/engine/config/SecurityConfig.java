9e08030922c0816c8d9417c99a0aa71e8b606ea9
micnori <micnori@users.noreply.github.com>
2025-11-20 14:19:53 +0100

    conf cors

--- src/main/java/eu/fbk/dslab/smartera/engine/config/SecurityConfig.java
+++ src/main/java/eu/fbk/dslab/smartera/engine/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package eu.fbk.dslab.smartera.engine.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -16,9 +20,19 @@ public class SecurityConfig {
                 .requestMatchers("/api/**").authenticated()
                 .anyRequest().permitAll()
             )
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-            .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.disable());
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+            //.csrf(csrf -> csrf.disable())
+            //.cors(cors -> cors.disable());
         return http.build();
+    }
+
+    @Bean 
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("*"));
+	    configuration.setAllowedMethods(Arrays.asList("*"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
     }
 }
