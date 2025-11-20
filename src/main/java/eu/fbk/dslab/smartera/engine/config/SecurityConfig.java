e29093d35d6f55d0a93f29226b4f8ee83f37dcdd
micnori <micnori@users.noreply.github.com>
2025-11-20 17:23:26 +0100

    another cors config

--- src/main/java/eu/fbk/dslab/smartera/engine/config/SecurityConfig.java
+++ src/main/java/eu/fbk/dslab/smartera/engine/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package eu.fbk.dslab.smartera.engine.config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -24,15 +20,5 @@ public class SecurityConfig {
             //.csrf(csrf -> csrf.disable())
             //.cors(cors -> cors.disable());
         return http.build();
-    }
-
-    @Bean 
-    UrlBasedCorsConfigurationSource corsConfigurationSource() {
-	    CorsConfiguration configuration = new CorsConfiguration();
-	    configuration.setAllowedOrigins(Arrays.asList("*"));
-	    configuration.setAllowedMethods(Arrays.asList("*"));
-	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    source.registerCorsConfiguration("/**", configuration);
-	    return source;
     }
 }
